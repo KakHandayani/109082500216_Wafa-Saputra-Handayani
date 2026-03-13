4d9ee88eca2bb926bc2002fd479480365fbc7784
KakHandayani <wafarental12@gmail.com>
2026-03-13 13:43:08 +0700

    deleted:    Week 3 - Modul 3/Guided/Guided3/Guided3.go 	modified:   Week 3 - Modul 3/Unguided/Unguided1/no1.go 	modified:   Week 4 - Modul 4/Guided/Guided1/Guided1.go 	deleted:    Week 4 - Modul 4/Guided/Guided3/Guided3.go

--- Week 4 - Modul 4/Guided/Guided1/Guided1.go
+++ Week 4 - Modul 4/Guided/Guided1/Guided1.go
@@ -5,17 +5,13 @@ import "fmt"
 func tambahvalue(x int) {
 	x = x + 10
 	fmt.Printf("Nilai x di dalam fungsi tambahvalue (pass by value): %d\n", x)
-
 }
-
 func tambahreference(x *int) {
 	*x = *x + 10
 	fmt.Printf("Nilai x di dalam fungsi tambahreference (pass by reference): %d\n", *x)
 }
-
 func main() {
 	var y int = 5
-
 	fmt.Println("nilai y awal: ", y)
 	tambahvalue(y)
 	fmt.Println("nilai x setelah pemanggilan tambahvalue: ", y)
