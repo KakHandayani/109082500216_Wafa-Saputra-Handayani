5264a9b5a27c46d5c55250824f0405d3ae3db53e
KakHandayani <wafarental12@gmail.com>
2026-03-31 19:18:50 +0700

    new file:   Week 5 - Modul 5/Guided/Guided1/Guided1.go 	new file:   Week 5 - Modul 5/Guided/Guided2/Guided2.go 	new file:   Week 5 - Modul 5/Guided/Guided3/Guided3.go 	new file:   Week 5 - Modul 5/Laprak Modul 5_Wafa Saputra Handayani.docx.pdf 	new file:   Week 5 - Modul 5/Modul 5 - Rekursif.pdf 	new file:   Week 5 - Modul 5/Unguided/Unguided1/Unguided1.go 	new file:   Week 5 - Modul 5/Unguided/Unguided2/Unguided2.go 	new file:   Week 5 - Modul 5/Unguided/Unguided3/Unguided3.go

---
+++ Week 5 - Modul 5/Unguided/Unguided3/Unguided3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func ganjil(i, n int) {
+	if i > n {
+		return
+	}
+	fmt.Print(i, " ")
+	ganjil(i+2, n)
+}
+func main() {
+	var n int
+	fmt.Print("Masukkan N: ")
+	fmt.Scan(&n)
+	ganjil(1, n)
+}
